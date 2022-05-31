7cd358a8388899c1c3183990df0cb724a5efb780
haracterwest <haracter.west@gmail.com>
2022-05-31 23:03:19 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,22 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+
+        //System.out.println(bean1.getMessage());
+
+        System.out.println(bean1 == bean2);
+
+        System.out.println(beanCat1 == beanCat2);
+    }
+}
